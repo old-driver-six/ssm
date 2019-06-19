c8297828cf5ec9c078e95dd3019b310205d4d757
sandaolgc <sandaolgc@163.com>
2019-06-19 17:01:58 +0800

    2019-6-19更新

---
+++ src/main/java/com/bj186/oas/service/personal/LeaveService.java
@@ -0,0 +1,19 @@
+package com.bj186.oas.service.personal;
+
+import com.bj186.oas.pojo.Leave;
+
+public interface LeaveService {
+    /**
+     * 添加请假表
+     */
+    Boolean insert(Leave leave);
+
+    /**
+     * 增加请假意见表,需要传递请假的条的id.
+     * @param opinion   请假意见
+     * @param state     请假状态值0为同意,1为不同意
+     * @Param lid       请假条id
+     * @return
+     */
+    Boolean updateLeave(String opinion,String state,String lid);
+}
