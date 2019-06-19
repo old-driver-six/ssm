27628cbfe5397e61297db2d9b7120ff9f6916be8
1187536461 <1187536461@qq.com>
2019-06-19 16:38:37 +0800

    ssm

--- src/main/java/com/bj186/oas/service/common/impl/NoticeServiceImpl.java
+++ src/main/java/com/bj186/oas/service/common/impl/NoticeServiceImpl.java
@@ -1,18 +1,14 @@
 package com.bj186.oas.service.common.impl;
 
 import com.bj186.oas.mapper.DepartmentMapper;
-import com.bj186.oas.mapper.StaffMapper;
-import com.bj186.oas.pojo.Announcements;
 import com.bj186.oas.pojo.Department;
 import com.bj186.oas.service.common.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 @Service("noticeService")
 public class NoticeServiceImpl implements NoticeService {
-    @Resource
+    @Autowired
     private DepartmentMapper departmentMapper;
     @Override
     public Department selectDepByKey(Integer id) {
